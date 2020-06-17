163ced02591a2d28440327975ffd899c0b578d79
Aaron Helton <mariusagricola@gmail.com>
2020-06-17 09:59:07 -0400

    include callback url

--- service.py
+++ service.py
@@ -5,6 +5,7 @@ ssm_client = boto3.client('ssm')
 connect_string = ssm_client.get_parameter(Name='connect-string')['Parameter']['Value']
 api_key = ssm_client.get_parameter(Name='undl-dhl-metadata-api-key')['Parameter']['Value']
 nonce_key = ssm_client.get_parameter(Name='undl-callback-nonce')['Parameter']['Value']
+callback_url = ssm_client.get_parameter(Name='undl-callback-url')['Parameter']['Value']
 
 # dlx-dl --connect=$UNDLFILES --type=bib --modified_within=8640 --preview
 
@@ -25,7 +26,8 @@ def handler(event, context):
                 modified_within=300,
                 api_key=api_key,
                 log=connect_string,
-                nonce_key=nonce_key
+                nonce_key=nonce_key,
+                callback_url=callback_url
             )
         return {
             'statusCode': 200,
