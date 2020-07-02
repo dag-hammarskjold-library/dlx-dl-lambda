ea75e957684f19591589bb2b09c155efd7bf6748
Aaron Helton <mariusagricola@gmail.com>
2020-07-02 14:58:12 -0400

    Revert "use concurrent threads for bibs and auths"

--- service.py
+++ service.py
@@ -1,6 +1,5 @@
 import dlx_dl
 import boto3
-from threading import Thread
 
 ssm_client = boto3.client('ssm')
 connect_string = ssm_client.get_parameter(Name='connect-string')['Parameter']['Value']
@@ -20,38 +19,22 @@ data = dlx_dl.LOG_DATA
 
 def handler(event, context):
     try:
-        Thread(
-            target=dlx_dl.main,
-            kwargs = {
-                'connect': connect_string,
-                'type': 'bib',
-                'modified_within': 300,
-                'api_key': api_key,
-                'log': connect_string,
-                'nonce_key': nonce_key,
-                'callback_url': callback_url,
-            }
-        ).start()
-    
-        Thread(
-            target= dlx_dl.main,
-            kwargs =  {
-                'connect': connect_string,
-                'type': 'auth',
-                'modified_within': 300,
-                'api_key': api_key,
-                'log': connect_string,
-                'nonce_key': nonce_key,
-                'callback_url': callback_url,
-            }
-        ).start()
-        
+        for coll in ['bib','auth']:
+            dlx_dl.main(
+                connect=connect_string,
+                type=coll,
+                modified_within=300,
+                api_key=api_key,
+                log=connect_string,
+                nonce_key=nonce_key,
+                callback_url=callback_url
+            )
         return {
             'statusCode': 200,
             'body': 'Update complete.'
         }
-    except Exception as e:
+    except:
         return {
             'statusCode': 500,
-            'body': str(e)
+            'body': 'Something went wrong.'
         }
